5cf9a826cab8c1391b11d233642c32b2602a40ed
efti-dot <eftih31@gmail.com>
2025-12-18 05:34:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from prompt import OpenAIConfig
 from dotenv import load_dotenv
 import os
 
-
-'''load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")'''
-
 api_key = st.secrets["OPENAI_API_KEY"]
 if not api_key:
     st.error("Please check the OPENAI_API_KEY.")
